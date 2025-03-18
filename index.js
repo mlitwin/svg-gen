41d9a3438b511719e8fcf9be5f6cb281277d7e05
Matthew Litwin <mlitwin@sonic.net>
2025-03-17 17:01:59 -0700

    WIP

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import svgGen from './src/svg-gen.js';
+import beautify from 'js-beautify';
 
 // Your code here to use svgGen
 const s = new svgGen({});
-const text = s.svg();
+const text = s.svg({},
+    s.text({ x: 10, y: 20 }, "Hello, World!").
+    circle({ cx: 50, cy: 50, r: 40 })
+).ret;
 
+const beautifiedText = beautify.html(text);
+console.log(beautifiedText);
 console.log(text);
