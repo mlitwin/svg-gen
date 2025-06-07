1ef2df278f8f7f3cc1916c023485cee79b3a6094
Matthew Litwin <mlitwin@sonic.net>
2025-06-07 16:16:35 -0700

    WIP

--- build/examples/hello-world.js
+++ build/examples/hello-world.js
@@ -2,11 +2,12 @@ import svgGen, {parseToText} from '@mlitwin/svg-gen';
 
 const s = new svgGen({});
 const svg = s.svg({}, [
+    s.title({ }, "Hello World with circle"),
     s.text({ x: 10, y: 20 }, "Hello, World!"),
     s.g({}, [
-        s.text({ x: 10, y: 20 }, "A")
+        s.text({ x: 10, y: 60 }, "A group")
     ]),
-    s.circle({ cx: 50, cy: 50, r: 40 })
+    s.circle({ cx: 50, cy: 50, r: 40, fill: "none", stroke: "black" })
 ]
 );
 
---
+++ build/scripts/make-examples.js
@@ -0,0 +1,120 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { exec } from 'child_process';
+import * as cheerio from 'cheerio';
+import Handlebars from "handlebars";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const examplesDir = path.resolve(__dirname, '../examples');
+const docsDir = path.resolve(__dirname, '../../docs/examples');
+const imgDir = path.join(docsDir, 'images');
+
+
+const manifest = {
+    examples: []
+};
+
+async function makeExample(file) {
+    console.log('Found JS file:', file);
+    const srcPath = path.join(examplesDir, file);
+
+    const outputFile = path.join(imgDir, file.replace(/\.js$/, '.svg'));
+
+    try {
+        const { stdout } = await new Promise((resolve, reject) => {
+            exec(`node "${srcPath}"`, (error, stdout, stderr) => {
+                if (error) reject(error);
+                else resolve({ stdout, stderr });
+            });
+        });
+
+        await fs.promises.writeFile(outputFile, stdout);
+        console.log(`Saved SVG output to ${outputFile}`);
+    } catch (err) {
+        console.error(`Error executing or writing SVG for ${file}:`, err);
+        return;
+    }
+
+    let title = "";
+    try {
+        title = (await getSVGTitle(outputFile)).trim();
+    } catch (err) {
+        console.error(`Error getting SVG title for ${file}:`, err);
+    }
+
+    const fileContents = await fs.promises.readFile(srcPath, 'utf8');
+
+
+    manifest.examples.push({
+        filename: file,
+        fileContents,
+        image: file.replace(/\.js$/, '.svg'),
+        title
+    });
+}
+
+async function getSVGTitle(file) {
+    try {
+        const data = await fs.promises.readFile(file, 'utf8');
+        const $ = cheerio.load(data, { xmlMode: true });
+        const titleEl = $('svg > title');
+        return titleEl.text();
+    } catch (err) {
+        console.error('Error reading file:', err);
+        return "";
+    }
+}
+
+
+const pageTemplate = Handlebars.compile(
+    `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Examples</title>
+</head>
+
+<body>
+    <div id="examples">
+    {{#each examples}}
+        <h2>{{title}}</h2>
+        <div class="example">
+            <img src="images/{{image}}">
+            <pre><code>{{fileContents}}</code></pre>
+        </div>
+    {{/each}}
+    </div>
+</body>
+
+</html>
+`
+);
+
+
+async function main() {
+    try {
+        const files = await fs.promises.readdir(examplesDir);
+
+        const jsFiles = files.filter(file => file.endsWith('.js'));
+        for (const file of jsFiles) {
+            await makeExample(file);
+        }
+
+        const page = pageTemplate(manifest);
+
+        await fs.promises.writeFile(
+            path.join(docsDir, 'index.html'),
+            page
+        );
+        console.log('Manifest written to', path.join(docsDir, 'manifest.json'));
+    } catch (err) {
+        console.error('Error:', err);
+        process.exit(1);
+    }
+}
+
+main();
